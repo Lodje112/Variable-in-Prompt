bbbdd310d984bf0b14edefda2be4bbfe02ebfaf4
Joel Jordan <99145296+Lodje112@users.noreply.github.com>
2022-05-12 12:48:26 -0300

    Add files via upload

---
+++ Js prompt var/aasdad.js
@@ -0,0 +1,29 @@
+var [num1, num2, num3, num4, num5] = prompt().split(" ").map(Number)
+
+        var maior = Math.max(num1, num2, num3, num4, num5,);
+
+        console.log(maior);
+
+/**     var [num1, num2]= prompt().split(" ").map(Number)
+        
+        var maior1 = Math.max(num1, num2)
+
+        console.log("o maior é " + maior1);
+        
+        var [num3] = prompt().split(" ").map(Number)
+        var maior2 = Math.max(maior1, num3)
+        
+        console.log("entre "+ maior1 + " e " + num3 + " o maior é " + maior2);
+
+        var [num4] = prompt().split(" ").map(Number)
+        var maior3 = Math.max(num4, maior2)
+        
+        console.log( "entre "+ maior2 + " e " + num4 + " o maior é " + maior3);
+
+        var [num5] = prompt().split(" ").map(Number)
+        var maior4 = Math.max(num5, maior3)
+
+        console.log("entre "+ maior3 + " e " + num5 + " o maior é " + maior4);
+
+
+ */
